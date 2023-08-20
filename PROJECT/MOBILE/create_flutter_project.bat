58c229c11f392eede439ab7c03da1ba87cf7a44b
senselogic <dev.senselogic@gmail.com>
2023-08-20 11:18:44 +0200

    % packages

--- PROJECT/MOBILE/create_flutter_project.bat
+++ PROJECT/MOBILE/create_flutter_project.bat
@@ -1,32 +1,35 @@
 call flutter create fluent_project --platforms=android,ios,web
 rename fluent_project CODE
 cd CODE
-rem call flutter pub add align_positioned
-rem call flutter pub add animations
-rem call flutter pub add auto_size_text
+call flutter pub add align_positioned
+call flutter pub add animations
+call flutter pub add auto_size_text
+rem call flutter pub add carbon_icons
+call flutter pub add cached_network_image
 call flutter pub add dio
+call flutter pub add drag_and_drop_lists
 rem call flutter pub add equatable
-rem call flutter pub add extra_alignments
-rem call flutter pub add file_picker
-rem call flutter pub add flextras
+rem call flutter pub add eva_icons_flutter
+call flutter pub add extra_alignments
+call flutter pub add file_picker
+call flutter pub add flextras
 call flutter pub add flutter_animate
 call flutter pub add flutter_bloc
-rem call flutter pub add flutter_drag_and_drop
-rem call flutter pub add flutter_form_builder
+rem call flutter pub add flutter_feather_icons
+call flutter pub add flutter_form_builder
 rem call flutter pub add flutter_local_notifications
 rem call flutter pub add flutter_markdown
 rem call flutter pub add flutter_native_splash
-rem call flutter pub add flutter_slidable
+call flutter pub add flutter_slidable
 call flutter pub add go_router
 rem call flutter pub add intro_slider
-rem call flutter pub add niku
+call flutter pub add niku
 rem call flutter pub add permission_handler
 rem call flutter pub add responsive_builder
 call flutter pub add shared_preferences
-rem call flutter pub add sized_context
-rem call flutter pub add sprintf
+call flutter pub add sized_context
 call flutter pub add supabase_flutter
-rem call flutter pub add textstyle_extensions
-rem call flutter pub add universal_platform
+rem call flutter pub add unicons
+call flutter pub add universal_platform
 rem call flutter pub add url_launcher
 pause
