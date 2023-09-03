fb666c68c27f3011f9ed06a11afebf48a5c38f56
senselogic <dev.senselogic@gmail.com>
2023-09-03 21:27:22 +0200

    + ts

--- PROJECT/WEB/create_sveltekit_project.bat
+++ PROJECT/WEB/create_sveltekit_project.bat
@@ -1,8 +1,9 @@
+@echo select Skeleton, TypeScript, ESLint
 call npm create svelte@latest fluent-project
 rename fluent-project CODE
 cd CODE
 call npm install --global yarn
 call yarn install
-call yarn add senselogic-gist @supabase/supabase-js
+call yarn add senselogic-flow senselogic-gist @supabase/supabase-js
 call yarn audit
 pause
